b5ca09e61ad9438a7f3fd291234d24a2f174350b
vtarabrin <v.tarabrin03@gmail.com>
2024-11-04 00:54:54 +0300

    finalx2

---
+++ precache-manifest.d94cdef3f983c9e8b58d24bbcfed6009.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ddef8a18f69a61cb0b52fa12285c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74fd06d18b0e825946d1",
+    "url": "/static/css/main.f86b462b.chunk.css"
+  },
+  {
+    "revision": "8dfd4d727553a1fa051f",
+    "url": "/static/js/2.790c68f7.chunk.js"
+  },
+  {
+    "revision": "2f58089c61d46f50cf6fed08f3e9f0eb",
+    "url": "/static/js/2.790c68f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74fd06d18b0e825946d1",
+    "url": "/static/js/main.f473cc12.chunk.js"
+  },
+  {
+    "revision": "0445e8fb01a5a4b47311",
+    "url": "/static/js/runtime-main.84a32411.js"
+  },
+  {
+    "revision": "73c00cad850662cc1c6f109b77846262",
+    "url": "/static/media/beton-plitka-kabel.73c00cad.jpg"
+  },
+  {
+    "revision": "10d8cd6120175c23d79f5911bc94be73",
+    "url": "/static/media/bitum-neft.10d8cd61.png"
+  },
+  {
+    "revision": "312e4c850b1fe560ddf2caa377c6ca13",
+    "url": "/static/media/danger.312e4c85.jpg"
+  },
+  {
+    "revision": "78b7e15ae7892f605c3e66375eb0e8be",
+    "url": "/static/media/ekspertnyi_1.78b7e15a.jpg"
+  },
+  {
+    "revision": "2a4f51040b1da222c4e1ac94685c7a8d",
+    "url": "/static/media/inspect-kolodec-zazem.2a4f5104.jpg"
+  },
+  {
+    "revision": "640377b026eb7328981969a6019b217c",
+    "url": "/static/media/jelez-zamer-stolbik.640377b0.jpg"
+  },
+  {
+    "revision": "5612fa698c6e78f37b9d5a30a1cd0ac7",
+    "url": "/static/media/kaska1.5612fa69.png"
+  },
+  {
+    "revision": "c908a8e676518b4e4cd4d861eee26a5e",
+    "url": "/static/media/kaska2.c908a8e6.png"
+  },
+  {
+    "revision": "d9737700a2d5bfde5f0a6944fdbbeb33",
+    "url": "/static/media/kolca-stenniye.d9737700.jpg"
+  },
+  {
+    "revision": "e7f4596682c65857c545186827f267c2",
+    "url": "/static/media/kolca.e7f45966.jpg"
+  },
+  {
+    "revision": "8734314770c4c105c55dbb391713a793",
+    "url": "/static/media/kolodec-zazem-bet.87343147.jpg"
+  },
+  {
+    "revision": "fe799b66e78848b94cf01be809ffc3eb",
+    "url": "/static/media/kriwka-kvadrat-kolodec.fe799b66.png"
+  },
+  {
+    "revision": "dff0344f9267d7f3797a18cd269528ba",
+    "url": "/static/media/kvadrat-kolodec.dff0344f.jpg"
+  },
+  {
+    "revision": "f2f295417062203290397702b9ca431a",
+    "url": "/static/media/lenta-signal.f2f29541.jpg"
+  },
+  {
+    "revision": "8d21932896d40a2ff6428abd8db88281",
+    "url": "/static/media/logo.8d219328.png"
+  },
+  {
+    "revision": "c3b8b9f7e1bd5257832bb11025e95bef",
+    "url": "/static/media/logo2.c3b8b9f7.jpg"
+  },
+  {
+    "revision": "6368d1ecfbcd75bae305a214903b32ff",
+    "url": "/static/media/luk-beton.6368d1ec.jpg"
+  },
+  {
+    "revision": "54ec0a153fc725c3bfee406d1e8d041c",
+    "url": "/static/media/luki-polimer.54ec0a15.jpg"
+  },
+  {
+    "revision": "42bddd0e1cd471bb225aab510f329ea2",
+    "url": "/static/media/main1.42bddd0e.jpg"
+  },
+  {
+    "revision": "87ee46b3f8566b4c624c04d4012cb104",
+    "url": "/static/media/main2.87ee46b3.jpg"
+  },
+  {
+    "revision": "7b84224b2b26f177d9dc056e78e2d4c8",
+    "url": "/static/media/main3.7b84224b.jpg"
+  },
+  {
+    "revision": "60b4c1a7bec006539f14b609fe4f8935",
+    "url": "/static/media/pdk10.60b4c1a7.webp"
+  },
+  {
+    "revision": "af934cc5fe3595ceda305d76d46869a1",
+    "url": "/static/media/pdk15.af934cc5.webp"
+  },
+  {
+    "revision": "af934cc5fe3595ceda305d76d46869a1",
+    "url": "/static/media/pdk20.af934cc5.webp"
+  },
+  {
+    "revision": "def4a40f246a1b75071f03da24e87f92",
+    "url": "/static/media/plita-beton.def4a40f.png"
+  },
+  {
+    "revision": "94b121162be7e9dbb80e58dafcc3ea2f",
+    "url": "/static/media/polog-bresent-voda.94b12116.jpg"
+  },
+  {
+    "revision": "30e1f6a4ebd6a9ba8d2d1e0888eb1e07",
+    "url": "/static/media/pp10-2.30e1f6a4.jpg"
+  },
+  {
+    "revision": "6f0552296b6099f081485d4c97600445",
+    "url": "/static/media/pp10.6f055229.jpg"
+  },
+  {
+    "revision": "642aca7f8f2a4f086db08aeee272da31",
+    "url": "/static/media/pp15.642aca7f.jpg"
+  },
+  {
+    "revision": "a4413c434f1e6fffd406eeb16642801b",
+    "url": "/static/media/pp20.a4413c43.jpg"
+  },
+  {
+    "revision": "77b87765394e9f9f0aa58995f6538034",
+    "url": "/static/media/pzk.77b87765.png"
+  },
+  {
+    "revision": "5c3c3df269e3fdbebc5d64b0ee9ec85f",
+    "url": "/static/media/tablica-marker-stolb.5c3c3df2.png"
+  },
+  {
+    "revision": "c1a3e68a65131557daded48e4ee23647",
+    "url": "/static/media/tent-tarpaulin-120.c1a3e68a.jpg"
+  },
+  {
+    "revision": "e9969dbad929d8b8df7913fde365bcd1",
+    "url": "/static/media/tent-tarpaulin-180.e9969dba.jpg"
+  },
+  {
+    "revision": "995a0bb1c3f1105d60b8f6e355d2fb1e",
+    "url": "/static/media/tent-tarpaulin-70.995a0bb1.jpg"
+  },
+  {
+    "revision": "4d27cc743c3af3eebcff4b10978212c6",
+    "url": "/static/media/vik_baze.4d27cc74.jpg"
+  },
+  {
+    "revision": "208d63aa35429367c293a3b4fb71b694",
+    "url": "/static/media/vik_expertnk.208d63aa.jpg"
+  },
+  {
+    "revision": "120efb5a44c168023e8a198cca7010fa",
+    "url": "/static/media/vik_nax.120efb5a.jpg"
+  },
+  {
+    "revision": "87ef5c2e684bb2b3875c099a15caef11",
+    "url": "/static/media/vik_transneft.87ef5c2e.jpg"
+  },
+  {
+    "revision": "4847a5bbc07e5b0783c52c89ca099441",
+    "url": "/static/media/vik_uni.4847a5bb.jpg"
+  },
+  {
+    "revision": "356285d62c624f871144ba263bd0f5eb",
+    "url": "/static/media/ybk5.356285d6.jpg"
+  },
+  {
+    "revision": "b15807e09665b382d51dfa56126b5d0e",
+    "url": "/static/media/zamer-stolb-24.b15807e0.jpg"
+  }
+]);
